6d4adbf308f3af22d3b9be9969584dd3be1b0cdf
harfool gujjar <=>
2025-02-03 20:13:10 +0530

    register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,16 +3,25 @@ import HomePage from "./pages/HomePage"
 import Login from "./pages/LoginPage"
 import RegisterPage from "./pages/RegisterPage"
 import Navbar from "./components/Navbar"
+import { useMemo } from "react"
+import { themeSettings } from "./pages/Theme"
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { Toaster } from "react-hot-toast"
 function App() {
-
+  const theme = useMemo(() => createTheme(themeSettings()), []);
   return (
     <>
+    <ThemeProvider  theme={theme}>
+      <CssBaseline/>
     <Navbar/>
+    <Toaster/>
     <Routes>
       <Route path="/" element={ <HomePage/>} />
       <Route path="/login" element={<Login/>} />
       <Route path="/register" element={<RegisterPage/> } />
     </Routes>
+    </ThemeProvider>
     </>
   )
 }
--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,8 +1,118 @@
+import {
+  Alert,
+  Box,
+  Button,
+  Collapse,
+  TextField,
+  Typography,
+  useMediaQuery,
+  useTheme,
+} from "@mui/material";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import toast from "react-hot-toast";
+import axios from "axios";
 
 const RegisterPage = () => {
-  return (
-    <div>RegisterPage</div>
-  )
-}
+  const theme = useTheme();
+  const navigate = useNavigate();
+  const isNotMobile = useMediaQuery("(min-width : 1000px)");
+  const [userName, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
 
-export default RegisterPage
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await axios.post("http://localhost:3000/api/v1/auth/register", { userName, email, password });
+      toast.success("User registered successfully");
+      setTimeout(() => {
+        navigate("/login");
+      }, 1500);
+    } catch (err) {
+      console.log(err);
+      if (err.response?.data?.error) {
+        setError(err.response.data.error);
+      } else if (err.message) {
+        setError(err.message);
+      }
+      setTimeout(() => {
+        setError("");
+      }, 5000);
+    }
+  };
+
+  return (
+    <Box
+      width={isNotMobile ? "40%" : "80%"}
+      p={"2em"}
+      m={"2em auto"}
+      borderRadius={5}
+      sx={{ boxShadow: 5 }}
+      backgroundColor={theme.palette.background.alt}
+    >
+      <Collapse in={!!error}>
+        <Alert severity="error" sx={{ mb: 2 }}>
+          {error}
+        </Alert>
+      </Collapse>
+      <form onSubmit={handleSubmit}>
+        <Typography variant="h3">Sign Up</Typography>
+        <TextField
+          id="username"
+          label="Username"
+          required
+          type="text"
+          margin="normal"
+          fullWidth
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+          aria-label="Enter your username"
+        />
+        <TextField
+          id="email"
+          label="Email"
+          type="email"
+          required
+          margin="normal"
+          fullWidth
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          aria-label="Enter your email"
+        />
+        <TextField
+          id="password"
+          label="Password"
+          type="password"
+          required
+          margin="normal"
+          fullWidth
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          aria-label="Enter your password"
+        />
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          size="large"
+          sx={{ mt: 2 }}
+        >
+          Submit
+        </Button>
+        <Typography mt={2}>
+          Already have an account?{" "}
+          <Link
+            to="/login"
+            style={{ textDecoration: "none", color: theme.palette.primary.main }}
+          >
+            Please login
+          </Link>
+        </Typography>
+      </form>
+    </Box>
+  );
+};
+
+export default RegisterPage;
